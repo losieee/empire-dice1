08bb1fe1a5aeff4521d880f69cfccb726f17323a
Choihyungwook1128 <ejyong1128@naver.com>
2025-11-23 18:09:32 +0900

    [Feat] 연결 테스트

---
+++ Unity/Assets/Scripts/basicProtocols.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicProtocols : MonoBehaviour
+{
+    public class packets
+    {
+        public class res
+        {
+            public string message;
+        }
+    }
+}
